86c17fdb7fa3e2db897b19f92931a3c07c57ebca
Liam Cox <liamcox@Liams-MacBook-Pro.local>
2020-12-28 14:05:05 -0600

    connected database

--- server/app.js
+++ server/app.js
@@ -1,8 +1,25 @@
+require("dotenv").config();
+
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+const db_url = process.env.DB_URL;
 
 const app = express();
+
+mongoose.connect(db_url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
 
 // bind express with graphql
 app.use(
